4cc74576a109c917ce803a1d19a00bf3f5d60b90
Trenerov <sustanon92@gmail.com>
2026-03-05 23:24:35 +0300

    fix: explicit vite output to root public folder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: '.',
+    build: {
+        outDir: '../public',
+        emptyOutDir: true,
+    },
     server: {
         port: 5173,
         proxy: {
